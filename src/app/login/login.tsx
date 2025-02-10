3526d3c0a5a65c23fba322ba6f7369533abeb780
Rafael Duarte Daltio <m320000@alunos.ipb.pt>
2025-02-10 03:43:11 -0300

    fix: adjustments at login page

--- src/app/login/login.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function LoginPage() {
-    return (
-        <div className="flex min-h-screen items-center justify-start bg-[#F9F9F9] w-full"></div>
-    );
-}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function LoginPage() {
     return (
         <div className="flex min-h-screen items-center justify-start bg-[#F9F9F9] w-full">
-            <div className="bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.15)] p-8 w-96 ml-20 z-10">
+            <div className="bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.15)] p-8 w-full sm:w-96 lg:w-[450px] ml-20 z-10">
                 <div className="flex justify-center mb-6">
                     <Image
                         src="/imgs/assets/logo.png"
@@ -14,14 +14,14 @@ export default function LoginPage() {
                 </div>
                 <form className="space-y-4">
                     <div>
-                        <label className="block text-sm font-medium text-[#4A4A4A] mb-1">
+                        <label className="block text-sm font-semibold text-[#4A4A4A] mb-1">
                             Email
                         </label>
                         <div className="flex items-center border border-black bg-[#F9F9F9] shadow-sm px-4 py-2">
                             <input
                                 type="email"
-                                placeholder="seunome@email.com"
-                                className="flex-grow bg-transparent focus:outline-none"
+                                placeholder="example@email.com"
+                                className="flex-grow bg-transparent focus:outline-none text-[#4A4A4A]"
                             />
                             <div className="ml-2">
                                 <Image
@@ -35,45 +35,59 @@ export default function LoginPage() {
                         </div>
                     </div>
 
-                    <div>
-                        <label className="block text-sm font-medium text-[#4A4A4A] mb-1">
-                            Password
-                        </label>
-                        <div className="flex items-center border border-black bg-[#F9F9F9 shadow-sm px-4 py-2">
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                className="flex-grow bg-transparent focus:outline-none"
-                            />
-                            <div className="ml-2">
-                                <Image
-                                    src="/imgs/assets/lock.png"
-                                    alt="Lock Icon"
-                                    width={16}
-                                    height={16}
-                                    className="object-contain"
+                    <div className="space-y-2">
+                        <div>
+                            <label className="block text-sm font-semibold text-[#4A4A4A] mb-1">
+                                Password
+                            </label>
+                            <div className="flex items-center border border-black bg-[#F9F9F9] shadow-sm px-4 py-2">
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    className="flex-grow bg-transparent focus:outline-none text-[#4A4A4A]"
                                 />
+                                <div className="ml-2">
+                                    <Image
+                                        src="/imgs/assets/lock.png"
+                                        alt="Lock Icon"
+                                        width={16}
+                                        height={16}
+                                        className="object-contain"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="space-y-8">
+                            <div className="flex items-center">
+                                <input 
+                                    type="checkbox"
+                                    id="show-password"
+                                    className="w-4 h-4 border border-black bg-[#F9F9F9] shadow-sm focus:outline-none mr-2"
+                                 />
+                                <label htmlFor="show-password" className="text-sm text-[#4A4A4A] font-light">
+                                    Mostrar a senha.
+                                </label>
+                            </div>
+                            <div className="space-y-8">
+                                <a href="#" className="text-[#4A4A4A] text-sm block">
+                                    Problemas para acessar sua conta?
+                                </a>
+                                <button className="w-full bg-gradient-to-r from-[#AE23A9] to-[#DC4E1B] text-white py-2">
+                                    Acessar
+                                </button>
                             </div>
                         </div>
                     </div>
 
-                    <div className="flex items-center">
-                        <input type="checkbox" id="show-password" className="mr-2" />
-                        <label htmlFor="show-password" className="text-sm text-[#4A4A4A]">
-                            Mostrar a senha.
-                        </label>
+                    <div className="flex items-center my-4">
+                        <div className="flex-grow border-t border-gray-400"></div>
+                        <span className="mx-4 text-sm text-[#4A4A4A]">ou</span>
+                        <div className="flex-grow border-t border-gray-400"></div>
                     </div>
-                    <a href="#" className="text-orange-500 text-sm">
-                        Problemas para acessar sua conta?
-                    </a>
-                    <button className="w-full bg-gradient-to-r from-[#AE23A9] to-[#DC4E1B] text-white py-2">
-                        Acessar
-                    </button>
-                    <div className="text-center text-sm text-[#4A4A4A]">ou</div>
                     <button className="w-full border border-gray-300 py-2 text-[#4A4A4A]">
                         Cadastrar
                     </button>
-                    <div className="text-center text-xs text-[#4A4A4A]">
+                    <div className="text-center text-xs text-[#4A4A4A] font-light">
                         <a href="#">Termos de uso</a> · <a href="#">Política de privacidade</a>
                     </div>
                 </form>
@@ -83,12 +97,12 @@ export default function LoginPage() {
                 <Image
                     src="/imgs/assets/bg.jpg"
                     alt="Imagem de fundo"
-                    layout="responsive"
-                    width={800}
-                    height={600}
-                    className="object-cover h-full"
+                    width={1200}
+                    height={800}
+                    className="object-cover w-full h-full"
                 />
             </div>
+
         </div>
     );
 }
